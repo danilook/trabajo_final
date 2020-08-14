91ef16be8b28bd8b05990b5cc405bc77c4ea10e4
Danilo Leon <daniloleon15@gmail.com>
2020-08-14 09:51:02 -0300

    commit #6 , se creo la clase PERSONA en app clientes

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-
+class  Persona(models.Model): 
+   id_persona = models.AutoField (primary_key= True) 
+   nombre = models.CharField(max_length = 220, blank = False, null = False)
+   apellido = models.CharField(max_length = 220, blank = False, null = False)
+   dni = models.IntegerField 
+   direccion = models.CharField(max_length = 220, blank = False, null = False)
+   correo = models.CharField(max_length = 100, blank = False, null = False)
+   edad = models.IntegerField 
