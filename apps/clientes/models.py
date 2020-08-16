829ad17995d4c6bc9a84b191d5fdee19b9a4a8a5
Danilo Leon <daniloleon15@gmail.com>
2020-08-16 10:17:52 -0300

    commit, se agregaron los atributos EDAD Y DNI en la clase PERSONA

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -4,7 +4,7 @@ class  Persona(models.Model):
    id_persona = models.AutoField (primary_key= True) 
    nombre = models.CharField(max_length = 220, blank = False, null = False)
    apellido = models.CharField(max_length = 220, blank = False, null = False)
-   dni = models.IntegerField 
+   dni = models.IntegerField ()
    direccion = models.CharField(max_length = 220, blank = False, null = False)
    correo = models.CharField(max_length = 100, blank = False, null = False)
-   edad = models.IntegerField 
+   edad = models.IntegerField ()
