254d42d4307fd6b4b98eb39dacb1d9c77e5f6e13
Danilo Leon <daniloleon15@gmail.com>
2020-08-21 11:57:13 -0300

    commit #12, se migro el modelo cliente en la app CLIENTES

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -8,3 +8,8 @@ class  Persona(models.Model):
    direccion = models.CharField(max_length = 220, blank = False, null = False)
    correo = models.CharField(max_length = 100, blank = False, null = False)
    edad = models.IntegerField ()
+
+
+class Cliente (Persona) : 
+   id_cliente = models.AutoField (primary_key= True)
+      
