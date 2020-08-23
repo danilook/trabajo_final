6e12f53a3247968d8a7aa3e67913bfc0393890fe
Danilo Leon <daniloleon15@gmail.com>
2020-08-23 20:13:09 -0300

    commit #25, se migro el modelo REPUESTO

--- apps/repuestos/models.py
+++ apps/repuestos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Repuesto (models.Model) : 
+    id_repuesto = models.AutoField (primary_key= True)
+    nombre = models.CharField ('nombre',max_length= 200, blank= False, null= False)
+    stock = models.IntegerField(default= 0 )
+    categoria = models.CharField ('categoria', max_length= 200, blank= False, null= False)
+   
