79235b0cee826f641313735655e13455d012a7b6
Danilo Leon <daniloleon15@gmail.com>
2020-08-23 11:09:48 -0300

    commit #16, se creo la clase empleado app/empleados/models.py

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -8,9 +8,7 @@ class  Persona(models.Model):
    direccion = models.CharField(max_length = 220, blank = False, null = False)
    correo = models.CharField(max_length = 100, blank = False, null = False)
    edad = models.IntegerField ()
-   class meta :
-      abstract= True
+   
 
 class Cliente (Persona) : 
    id_cliente = models.AutoField (primary_key= True)
-      
--- apps/empleados/models.py
+++ apps/empleados/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from clientes.models import Persona
 
-class Empleado(Persona): 
-   id_empleado = models.AutoField (primary_key= True) 
-   rol = models.CharField ( max_length = 200 , blank= False, null= False) 
-   fecha_alta = models.DateField(blank= False, null= False)
-   reputacion = models.IntegerField (blank= False, null= False)
+
+class Empleado(models.Model): 
+  id_empleado = models.AutoField (primary_key= True) 
+  rol = models.CharField ( max_length = 200 , blank= False, null= False) 
+  fecha_alta = models.DateField(blank= False, null= False)
+  reputacion = models.IntegerField (blank= False, null= False)
+
