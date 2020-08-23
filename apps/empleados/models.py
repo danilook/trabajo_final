8069176fac05694da970f1d655cb2fc4b73882b7
Danilo Leon <daniloleon15@gmail.com>
2020-08-23 12:44:34 -0300

    commit #19, se migro modelo empleados

--- apps/empleados/models.py
+++ apps/empleados/models.py
@@ -4,10 +4,10 @@ import datetime
 
 class Empleado(models.Model): 
   id_empleado = models.AutoField (primary_key = True)
-  nombre = models.CharField ('nombre', max_length = 200, blank = False, null = False) 
-  apellido = models.CharField ('apellido', max_length = 250, blank = False, null = False)
-  dni = models.IntegerField (blank = False, null = False)
-  rol = models.CharField ('rol', max_length = 200 , blank = False, null = False) 
+  nombre = models.CharField (default='', max_length = 200, blank = False, null = False) 
+  apellido = models.CharField (default='', max_length = 250, blank = False, null = False)
+  dni = models.IntegerField (blank = True, null = True)
+  rol = models.CharField (default= '', max_length = 200 , blank = False, null = False) 
   fecha_alta = models.DateField(blank = False, null = False)
   reputacion = models.IntegerField (blank = False, null = False)
 
