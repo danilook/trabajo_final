7d20bf1f7c2a7f087c0605a69ab353954e095c33
Danilo Leon <daniloleon15@gmail.com>
2020-08-23 14:19:40 -0300

    commit #23, se migro el modelo proveedor y se modifico app/proveedores/admin.py

--- apps/proveedores/models.py
+++ apps/proveedores/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Proveedor (models.Model):
+    id_proveedor = models.AutoField (primary_key= True)
+    nombre= models.CharField (max_length = 200, blank= False, null= False)
+    apellido = models.CharField (max_length= 200, blank= False, null= False)
+    dni = models.IntegerField ()
+    categoria = models.CharField (max_length= 250, blank= False, null= False)
+    telefono = models.IntegerField ()
+    correo = models.CharField (max_length=100 , blank= False, null= False)
+    direccion = models.CharField (max_length= 250, blank= False, null= False)
