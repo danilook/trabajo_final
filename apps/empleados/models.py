c445eddacd14f5b92e2af849c82400a28d74f7b9
Danilo Leon <daniloleon15@gmail.com>
2020-08-23 11:23:07 -0300

    commit #18, se modifico  apps/empleados/ models.py

--- apps/empleados/models.py
+++ apps/empleados/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+import datetime 
 
 
 class Empleado(models.Model): 
-  id_empleado = models.AutoField (primary_key= True) 
+  id_empleado = models.AutoField (primary_key = True)
+  nombre = models.CharField (max_length = 200, blank= False, null= False) 
+  apellido = models.CharField (max_length =250, blank= False, null= False)
+  dni = models.IntegerField (blank= False, null= False)
   rol = models.CharField ( max_length = 200 , blank= False, null= False) 
   fecha_alta = models.DateField(blank= False, null= False)
   reputacion = models.IntegerField (blank= False, null= False)
