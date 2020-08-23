cd6332b0fec41db8a0a5cbfa24cc70ce32d07fe7
Danilo Leon <daniloleon15@gmail.com>
2020-08-23 20:26:48 -0300

    commit #26, se modifico app/repuestos/models.py

--- apps/repuestos/models.py
+++ apps/repuestos/models.py
@@ -5,4 +5,10 @@ class Repuesto (models.Model) :
     nombre = models.CharField ('nombre',max_length= 200, blank= False, null= False)
     stock = models.IntegerField(default= 0 )
     categoria = models.CharField ('categoria', max_length= 200, blank= False, null= False)
-   
+    class Meta:
+       verbose_name = 'Repuesto'
+       verbose_name_plural = 'Repuestos'
+       ordering = ['nombre']
+
+    def __str__ (self):
+       return self.nombre  
