95eb553618aa316b53406b55b6297b0569bb6365
Danilo Leon <daniloleon15@gmail.com>
2020-08-23 20:40:44 -0300

    commit #27, se migro el modelo TRABAJO

--- apps/trabajos/models.py
+++ apps/trabajos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Trabajo (models.Model) : 
+    id_trabajo = models.AutoField (primary_key= True)
+    tipo_trabajo = models.CharField ('Tipo de trabajo', max_length= 300 , blank=False, null= False)
+    fecha_inicio = models.DateField ('Fecha de inicio' , blank= False, null= False )
+    fecha_finalizacion = models.DateField ( 'Fecha de finalizacion', blank= False, null= False)
+    detalle = models.CharField ('Detalle de trabajo', max_length= 500, blank=True, null= True)
+    estado = models.CharField ('Estadp de trabajo', max_length= 50, blank= False, null= False)
+
+    class Meta:
+        verbose_name = 'Trabajo'
+        verbose_name_plural = 'Trabajos'
+        ordering = ['nombre']
